af0531e5bebd756d6ead974cd27eb8f9ffdebfd4
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-06-16 21:00:50 -0400

    notes save to db, need to refresh on save, working on delete function

--- server.js
+++ server.js
@@ -1,39 +1,106 @@
-//npm run dev <----loads the server in dev mode
-//npm start <----loads the server in prod mode
+// DEV MODE  = npm run dev 
+// PROD MODE = npm start 
 
-//setup sever instance
+// REQUIREMENTS
 const express = require('express');
 const path = require('path');
 const notesDB = require('./db/db.json');
 const app = express();
-//setup port
+const moment = require('moment');
+const fs = require('fs');
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}:// ${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+    next();
+};
+
+ // API'S
+ // SETS UP API FOR NOTES
+ app.get('/api/notes', (req, res) => res.json(notesDB));
+
+
+// //SETS ERROR CODE
+// app.use((req, res, next) => {
+//     res.status(404).send("YOUR PRINCESS IS IN ANOTHER CASTLE...༼ つ ◕_◕ ༽つ");
+// })
+
+
+// PORT CONFIGURATION
+// PORT NUMBER
 const port = process.env.PORT || 3001;
-
-//Show port that server started on
+// SHOW LISTENING PORT ON SERVER CONSOLE
 app.listen(port, () => console.log(`Listening on port ${port}`));
-// Middleware to set static folder
-app.use(express.static(path.join(__dirname, 'public')));
 
-//setting up the api
-app.get('/api/notes', (req, res) => res.json(notesDB));
-//Homepage
+
+
+
+// MIDDLEWARE
+// USE LOGGER 
+app.use(logger);
+// SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+//URL ROUTING
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+// ROUTES
+// TO INDEX
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
-//Main routes
+// TO NOTES
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))  
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
-//WILDCARD
+// TO WILDCARD - ERROR HANDLER
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/404.html')),
-    //set error status
-    res.status(404),
-res.send('None shall pass')
+    res.sendFile(path.join(__dirname, 'public/404.html'))
 );
 
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
 
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
 
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            note_id: uuid()
+        };
+
+        // Obtain existing reviews
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new review
+                parsedNotes.push(newNote);
+
+                // Write updated reviews back to the file
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated reviews!')
+                );
+            }
+        })
+    }
+});
 
 // WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left - hand column, plus empty fields to enter a new note title and the note’s text in the right - hand column
